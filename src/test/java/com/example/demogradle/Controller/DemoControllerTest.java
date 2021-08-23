8699b3c748b59ef52013012533344a17df123d8c
cystal-dot <87904880+cystal-dot@users.noreply.github.com>
2021-08-23 23:34:32 +0900

    Add files via upload

---
+++ src/test/java/com/example/demogradle/Controller/DemoControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.demogradle.Controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.demogradle.controller.DemoController;
+import org.junit.jupiter.api.Test;
+
+public class  DemoControllerTest{
+
+    public DemoController target = new DemoController();
+
+    @Test
+    public void indexの戻り値テスト() {
+        String actual = target.index();
+        assertEquals("index",actual);
+    }
+}
